8c4f92d6bb65cffc6c8dccb861a4035c20360ea3
santiagorejas <santiago.orejasrg@gmail.com>
2022-07-19 14:15:19 -0300

    animated logo added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Logo from "./components/UI/Logo/Logo";
 
 function App() {
   return (
     <div className="App">
+      <Logo />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
